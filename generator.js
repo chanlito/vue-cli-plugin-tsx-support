1aaace242e2c40e0186654b18a313e5dc48bbede
Chanlito <chanlitohem@gmail.com>
2019-04-16 10:59:57 +0700

    refactor: :recycle: upgrade version & refactoring
    - upgrade vue-tsx-support version
    - refactor how `vue-tsx-support/enable-check` is imported

--- generator.js
+++ generator.js
@@ -2,7 +2,7 @@
 module.exports = (api, options, rootOptions) => {
   api.extendPackage({
     dependencies: {
-      'vue-tsx-support': '^2.1.1',
+      'vue-tsx-support': '^2.3.0',
     },
     devDependencies: {
       'vue-jsx-hot-loader': '^1.4.0',
@@ -10,21 +10,11 @@ module.exports = (api, options, rootOptions) => {
   });
 
   api.postProcessFiles(files => {
-    const tsconfig = 'tsconfig.json';
-    if (files[tsconfig]) {
-      const tsconfigJSON = JSON.parse(files[tsconfig]);
-      tsconfigJSON.compilerOptions.types = [
-        ...new Set([
-          'vue-tsx-support/enable-check',
-          ...(tsconfigJSON.compilerOptions.types || []),
-        ]),
-      ];
-      files[tsconfig] = JSON.stringify(tsconfigJSON, null, 2);
-    }
-
     const shimsTSX = 'src/shims-tsx.d.ts';
     if (files[shimsTSX]) {
       const lines = files[shimsTSX].split(/\r?\n/g);
+      // import `vue-tsx-suppport` check
+      lines.unshift(`import 'vue-tsx-support/enable-check';`, '');
       const index = lines.findIndex(i => i.indexOf('[elem: string]: any') > 0);
       // remove vue `shims-tsx` default index signature
       // because 'vue-tsx-support/enable-check' already has it.
